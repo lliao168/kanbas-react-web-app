35329de866e4c4861c08f849652764f5f47d1fe3
lliao168 <louiseliao0802@gmail.com>
2024-04-05 00:24:08 -0400

    a6 updates

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -32,10 +32,10 @@ import { assignments } from "../Database";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+const API_BASE = process.env.REACT_APP_API_BASE;
 function Courses() {
         const { courseId } = useParams();
-        const COURSES_API = "http://localhost:4000/api/courses";
+        const COURSES_API = `${API_BASE}/api/courses`;
         const [course, setCourse] = useState<any>({ _id: "" });
         const findCourseById = async (courseId?: string) => {
             const response = await axios.get(
