0e32fac168f8e0e7d61a76b745da55861742dcd6
lliao168 <louiseliao0802@gmail.com>
2024-04-05 00:16:52 -0400

    a5 REST fa23

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,26 +1,26 @@
 import axios from "axios";
 const API_BASE = process.env.REACT_APP_API_BASE;
 export const fetchAllCourses = async () => { 
-    const response = await axios.get("http://localhost:4000/api/courses");
+    const response = await axios.get(`${API_BASE}/api/courses`);
     return response.data;
 };
 
 export const fetchCourseById = async (id: string) => {
-    const response = await axios.get(`http://localhost:4000/api/courses/${id}`);
+    const response = await axios.get(`${API_BASE}/api/courses/${id}`);
     return response.data;
 }
 
 export const createCourse = async (course: any) => {
-    const response = await axios.post("http://localhost:4000/api/courses", course);
+    const response = await axios.post(`${API_BASE}/api/courses`, course);
     return response.data;
 };
 
 export const deleteCourse = async (id: string) => {
-    const response = await axios.delete(`http://localhost:4000/api/courses/${id}`);
+    const response = await axios.delete(`${API_BASE}/api/courses/${id}`);
     return response.data;
 };
 
 export const updateCourse = async (course: any, id: string) => {
-    const response = await axios.put(`http://localhost:4000/api/courses/${id}`, course);   
+    const response = await axios.put(`${API_BASE}/api/courses/${id}`, course);   
     return response.data;
 } 
