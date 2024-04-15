79f1b9d1e867cf389d91726b12e72b7f9546ecbc
lliao168 <louiseliao0802@gmail.com>
2024-04-15 16:06:55 -0400

    a6 Mongo su23

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -57,9 +57,9 @@ function Kanbas() {
     return (
     <Provider store={store}>
         <div className="d-flex">
-            <div>
-                <KanbasNavigation/>
-            </div>
+            
+            <KanbasNavigation/>
+            
             <div style={{ flexGrow: 1 }}>
                 {/* <h1>Account</h1>
                 <h1>Dashboard</h1>
